34a47e2cbad27b6b0ae52941395d20e9c4b1f273
zhanxuzhao <649912323@qq.com>
2026-06-22 00:33:40 +0800

    Remove projectile boundary check - infinite map, only lifetime controls death

--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -74,9 +74,6 @@ func _process(delta: float) -> void:
 		return
 
 	var pos = global_position
-	if pos.x < -500 or pos.x > 2000 or pos.y < -500 or pos.y > 1500:
-		queue_free()
-
 	queue_redraw()
 
 
