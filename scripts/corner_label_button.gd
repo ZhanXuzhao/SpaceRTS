0f76564f1280ee72273ceffda77f3da5555fec82
zhanxuzhao <649912323@qq.com>
2026-06-25 11:22:01 +0800

    rename Label1-4 to LabelTL/TR/BL/BR; remove unnecessary .uid file

--- scripts/corner_label_button.gd
+++ scripts/corner_label_button.gd
@@ -9,10 +9,10 @@ signal right_clicked
 var _bg: ColorRect
 var _border: ColorRect
 var _name_label: Label
-var _tl: Label   # top-left     (Label1)
-var _tr: Label   # top-right    (Label2)
-var _bl: Label   # bottom-left  (Label3)
-var _br: Label   # bottom-right (Label4)
+var _tl: Label   # top-left     (LabelTL)
+var _tr: Label   # top-right    (LabelTR)
+var _bl: Label   # bottom-left  (LabelBL)
+var _br: Label   # bottom-right (LabelBR)
 
 var _disabled: bool = false
 
@@ -21,10 +21,10 @@ func _ready() -> void:
 	_bg = $Bg
 	_border = $Border
 	_name_label = $Name
-	_tl = $Label1
-	_tr = $Label2
-	_bl = $Label3
-	_br = $Label4
+	_tl = $LabelTL
+	_tr = $LabelTR
+	_bl = $LabelBL
+	_br = $LabelBR
 
 
 func _gui_input(event: InputEvent) -> void:
@@ -59,34 +59,34 @@ func get_name_text() -> String:
 
 ## 左上角标
 func set_tl(t: String) -> void:
-	if _tl == null: _tl = $Label1
+	if _tl == null: _tl = $LabelTL
 	_tl.text = t
 func get_tl() -> Label:
-	if _tl == null: _tl = $Label1
+	if _tl == null: _tl = $LabelTL
 	return _tl
 
 ## 右上角标（技能 CD）
 func set_tr(t: String) -> void:
-	if _tr == null: _tr = $Label2
+	if _tr == null: _tr = $LabelTR
 	_tr.text = t
 func get_tr() -> Label:
-	if _tr == null: _tr = $Label2
+	if _tr == null: _tr = $LabelTR
 	return _tr
 
 ## 左下角标
 func set_bl(t: String) -> void:
-	if _bl == null: _bl = $Label3
+	if _bl == null: _bl = $LabelBL
 	_bl.text = t
 func get_bl() -> Label:
-	if _bl == null: _bl = $Label3
+	if _bl == null: _bl = $LabelBL
 	return _bl
 
 ## 右下角标（自动标签 / 费用）
 func set_br(t: String) -> void:
-	if _br == null: _br = $Label4
+	if _br == null: _br = $LabelBR
 	_br.text = t
 func get_br() -> Label:
-	if _br == null: _br = $Label4
+	if _br == null: _br = $LabelBR
 	return _br
 
 ## 禁用状态（变灰 + 忽略鼠标）
